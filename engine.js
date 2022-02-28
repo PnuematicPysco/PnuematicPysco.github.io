bebd53e5734c9fb0277669d2ade7317f02ebd6d8
PnuematicPysco <100618430+PnuematicPysco@users.noreply.github.com>
2022-02-28 11:49:49 -0800

    Create engine.js
    the meat and core of the engine. please, if you spot any problems let me know with a pull request.

---
+++ engine.js
@@ -0,0 +1,104 @@
+var monsterId = [];
+var monsterShot = [];
+var monsterX = [];
+var monsterZ = [];
+var x = 0;
+var z = 0;
+var rotate = 0;
+
+function fake3D(){
+var i;
+for(i = 0;i < monsterId.length;i++){
+if(monsterShot[i] == "notShot"){
+var monster = document.getElementById(monsterId[i]);
+if(monsterZ[i] > z){
+var graphicZ = monsterZ[i] - z - 500;//make move and pos (num * 5) to balance it out
+if(graphicZ > 0){
+monster.style.height = ""+graphicZ - graphicZ - graphicZ+"px";
+monster.style.width = ""+graphicZ - graphicZ - graphicZ+"px";
+}else{
+monster.style.height = ""+graphicZ + graphicZ + graphicZ+"px";
+monster.style.width = ""+graphicZ + graphicZ + graphicZ+"px";
+}
+monster.style.transform = "translateX("+x - monsterX[i] - rotate+"px)";
+}else{
+var graphicZ = monsterZ[i] - z + 500;//make move and pos (num * 5) to balance it out
+if(graphicZ > 0){
+monster.style.height = ""+graphicZ - graphicZ - graphicZ+"px";
+monster.style.width = ""+graphicZ - graphicZ - graphicZ+"px";
+}else{
+monster.style.height = ""+graphicZ + graphicZ + graphicZ+"px";
+monster.style.width = ""+graphicZ + graphicZ + graphicZ+"px";
+}
+var oppositeRotate = x - monsterX[i] - rotate;
+monster.style.transform = "translateX("+oppositeRotate + oppositeRotate + oppositeRotate+"px)";
+}
+}else{
+var monster = document.getElementById(monsterId[i]);
+monster.style.display = "none";
+}
+}
+}
+
+function shoot(){
+var i;
+for(i = 0;i < monsterId.length;i++){
+var monster = document.getElementById(monsterId[i]);
+var Rotate2 = x - monsterX[i] - rotate;
+var oppositeRotate = Rotate2 + Rotate2 + Rotate2;
+if(monsterZ[i] > z){
+var graphicZ = monsterZ[i] - z - 500;
+if(graphicZ > 0){
+if(Rotate2 > -10){
+if(Rotate2 < 10){
+if(monsterShot[i] == "notShot"){
+monsterShot[i] = "Shot";
+}}}}
+}else{
+var graphicZ = monsterZ[i] - z - 500;
+if(graphicZ > 0){
+if(oppositeRotate > -10){
+if(oppositeRotate < 10){
+if(monsterShot[i] == "notShot"){
+monsterShot[i] = "Shot";
+}}}}
+}
+}
+}
+
+
+document.addEventListener('keydown', (event) => {
+  var name = event.key;
+  var code = event.code;
+ 
+ if(name == "w"){
+ z = z + 50;
+ }
+ if(name == "a"){
+ x = x - 50;
+ }
+ if(name == "s"){
+ z = z - 50;
+ }
+ if(name == "d"){
+ x = x + 50;
+ }
+}, false);
+
+document.onkeydown = function (event) {
+      switch (event.keyCode) {
+         case 37:
+            rotate = rotate - 50;
+            if(rotate < -180 * 5){
+            rotate = 180 * 5;
+            }
+            break;
+         case 39:
+            rotate = rotate + 50;
+            if(rotate > 180 * 5){
+            rotate = -180 * 5;
+            }
+            break;
+        case 32:
+        shoot();
+      }};
